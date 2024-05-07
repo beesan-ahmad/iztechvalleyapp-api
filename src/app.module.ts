5f3de8f523f2231727bcb5bc0e2d8527ecc62568
beesan-ahmad <abumfarreh@gmail.com>
2024-05-07 17:47:34 +0430

    Initialized Mongoose Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule,
+    MongooseModule.forRoot
+      ('mongodb+srv://abumfarrehb:9cSZK*f92R$useK@cluster0.gtkc77o.mongodb.net/')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
